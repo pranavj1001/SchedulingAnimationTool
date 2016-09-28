fcfba6ba94e7b92b62252beccd81064ce3be27ad
pranavj1001 <pranavj1001@gmail.com>
2016-09-28 23:37:03 +0530

    updated the DataBase

--- sat.sql
+++ sat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 12, 2016 at 12:14 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: localhost
+-- Generation Time: Sep 25, 2016 at 05:29 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -48,17 +48,21 @@ INSERT INTO `login_table` (`email`, `password`) VALUES
 CREATE TABLE `mother_ship` (
   `id` int(11) NOT NULL,
   `latitude` float NOT NULL,
-  `longitude` float NOT NULL
+  `longitude` float NOT NULL,
+  `mother_ship_name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mother_ship`
 --
 
-INSERT INTO `mother_ship` (`id`, `latitude`, `longitude`) VALUES
-(1, 17.8674, 66.543),
-(2, 17.8674, 66.543),
-(3, 17.8674, 66.543);
+INSERT INTO `mother_ship` (`id`, `latitude`, `longitude`, `mother_ship_name`) VALUES
+(1, 17.8674, 66.543, 'Vikrant'),
+(2, 17.8674, 66.543, 'Chakra'),
+(3, 17.8674, 66.543, 'Hamla'),
+(4, 17.8674, 66.543, 'Kirpan'),
+(5, 17.8674, 66.543, 'Vinash'),
+(6, 17.8674, 66.543, 'Trishul');
 
 -- --------------------------------------------------------
 
@@ -70,18 +74,20 @@ CREATE TABLE `project` (
   `id` int(11) NOT NULL,
   `model_reference` varchar(30) DEFAULT NULL,
   `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `duration` time NOT NULL
+  `end_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `project`
 --
 
-INSERT INTO `project` (`id`, `model_reference`, `start_date`, `end_date`, `duration`) VALUES
-(1, 'project_1', '2016-08-01', '2016-09-30', '23:00:00'),
-(2, 'project_2', '2016-07-27', '2016-08-23', '21:01:01'),
-(3, 'project_3', '2016-07-01', '2016-08-31', '09:09:09');
+INSERT INTO `project` (`id`, `model_reference`, `start_date`, `end_date`) VALUES
+(1, 'Rock Dumping', '2016-08-01', '2016-09-30'),
+(2, 'Oil Extraction', '2016-07-27', '2016-08-23'),
+(3, 'Transport', '2016-07-01', '2016-08-31'),
+(4, 'Alternative Energy', '2016-07-18', '2016-10-30'),
+(5, 'Seabed Study', '2016-08-16', '2016-10-16'),
+(6, 'Pipelay', '2016-08-19', '2016-11-03');
 
 -- --------------------------------------------------------
 
